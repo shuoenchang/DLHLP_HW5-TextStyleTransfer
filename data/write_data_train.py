247d979b4c27d77a25b2caad3598ce06bd9b0812
MarvinChung <marvin852316497@gmail.com>
2020-04-11 20:43:37 +0800

    gender write_data_train_example

--- data/write_data_train.py
+++ data/write_data_train.py
@@ -2,14 +2,10 @@ def write_in(from_file, to_file, label):
     for line in from_file.readlines():
         to_file.write("__label__"+label+" "+line)
 to_file = open("data_train_gender.txt", "w") 
-imdb_pos = open("gender_data/train.pos","r")
-imdb_neg = open("gender_data/train.neg","r")
-yelp_pos = open("gender_data/train.pos","r")
-yelp_neg = open("gender_data/train.neg","r")
+gender_pos = open("gender_data/train.pos","r")
+gender_neg = open("gender_data/train.neg","r")
 
-write_in(imdb_pos, to_file, "pos")
-write_in(imdb_neg, to_file, "neg")
-write_in(yelp_pos, to_file, "pos")
-write_in(yelp_neg, to_file, "neg")
+write_in(gender_pos, to_file, "pos")
+write_in(gender_neg, to_file, "neg")
 
     
